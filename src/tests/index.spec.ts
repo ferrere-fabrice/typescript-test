a6e93713a121ff39b3e8639a3b0376a637d9336e
Fabrice FERRERE <ferrerefabrice@outlook.fr>
2023-06-22 09:20:04 +0200

    init project

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,19 @@
+/*
+QUAND on saisit une chaîne ALORS celle-ci est renvoyée en miroir
+QUAND on saisit un palindrome ALORS celui-ci est renvoyé ET « Bien dit » est envoyé ensuite
+QUAND on saisit une chaîne ALORS « Bonjour » est envoyé avant toute réponse
+QUAND on saisit une chaîne ALORS « Au revoir » est envoyé en dernier
+
+*/
+
+describe('Tester le comportement avancer du rover', () => {
+  it('Le rover doit avancer - doit être sur la deuxième case de la planète', () => {
+    // GIVEN - ARRANGE
+
+    // WHEN - ACT
+
+    // THEN - ASSERT
+    expect(true).toBe(true);
+  });
+});
+
